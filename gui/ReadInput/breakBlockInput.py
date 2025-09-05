c605062fba8a65818d1e492e46c7532fd769bb2f
mtbook_ncstate <mtbook@ncsu.edu>
2025-09-05 14:53:40 -0400

    fix break block 4

--- gui/ReadInput/breakBlockInput.py
+++ gui/ReadInput/breakBlockInput.py
@@ -2,6 +2,7 @@
 break1 = ""
 break2 = ""
 break3 = ""
+break4 = ""
 
 def read_break():
     try:
@@ -9,13 +10,15 @@ def read_break():
         with open('input_files/break_block_input.txt', 'r') as f:
             rows = [line.strip() for line in f.readlines() if line.strip()]
 
-            global break1, break2, break3
+            global break1, break2, break3, break4
             if len(rows) > 0:
                 break1 = rows[0]
             if len(rows) > 1:
                 break2 = rows[1]
             if len(rows) > 2:
                 break3 = rows[2]
+            if len(rows) > 3:
+                break4 = rows[3]
 
     except FileNotFoundError:
         print("file not found.")
