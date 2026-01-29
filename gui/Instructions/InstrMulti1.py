8eea966f9f5ddf588e668eb4470cb9d4316f55ab
mtbook_ncstate <mtbook@ncsu.edu>
2026-01-29 11:55:56 -0500

    fix multitasking instructions

--- gui/Instructions/InstrMulti1.py
+++ gui/Instructions/InstrMulti1.py
@@ -1,7 +1,6 @@
 from PyQt6.QtWidgets import (
     QApplication, QWidget, QLabel, QRadioButton, QGroupBox,
     QHBoxLayout, QVBoxLayout, QPushButton, QMainWindow, QSizePolicy
-    QHBoxLayout, QVBoxLayout, QPushButton, QMainWindow, QSizePolicy
 )
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont, QGuiApplication
